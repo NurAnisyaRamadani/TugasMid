241021663139eeeb90557cba1762e36e384e2638
NurAnisyaRamadani <127595237+NurAnisyaRamadani@users.noreply.github.com>
2023-03-29 16:01:01 +0700

    Add files via upload

---
+++ TestErr0042.java
@@ -0,0 +1,16 @@
+ /*Nama    : Nur Anisya Ramadani*/
+ /*Stambuk : 13020210042*/
+ /*Kelas   : B1*/
+ import java.util.Scanner;
+
+ public class TestErr0042 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        int k = 100;
+        char c = 's';
+        boolean falses = true;
+        int j = i + 1;
+        System.out.println("j adalah " + j + ", k adalah " + k + ", dan c adalah " + c + ", dan falses adalah " + falses);
+    }
+}
